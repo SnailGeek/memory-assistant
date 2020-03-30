21dbe719f6add3a1700c8bade853c4f26ecedccd
wangf-q <wangf-q@glodon.com>
2020-03-30 23:01:08 +0800

    create project

---
+++ src/main/java/com/memory/assistant/MemoryAssitantApplication.java
@@ -0,0 +1,13 @@
+package com.memory.assistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoryAssitantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemoryAssitantApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/memory/assistant/MemoryAssitantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.memory.assistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoryAssitantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
